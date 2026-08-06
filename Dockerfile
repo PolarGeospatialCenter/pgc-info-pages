5cc3d169ae60f36f779a73044c9af958f5429075
john4390 <john4390@umn.edu>
2026-08-06 14:16:35 -0500

    updating gulp and dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,22 @@
-FROM pgc-docker.artifactory.umn.edu/mirror/centos/nodejs-10-centos7
-ADD . .
-USER root
-RUN chown -R 1001:0 ${APP_ROOT}/src/dist
-USER 1001
-RUN npm install
-RUN gulp dist
+# Stage 1: Build the static assets
+FROM pgc-docker.artifactory.umn.edu/mirror/library/node:20-alpine AS builder
 
+WORKDIR /app
+
+# Copy dependency files first to leverage Docker layer caching
+COPY package*.json ./
+RUN npm ci
+
+# Copy source files and build production assets
+COPY . .
+RUN npx gulp dist
+
+# Stage 2: Serve static files with Nginx
 FROM pgc-docker.artifactory.umn.edu/mirror/nginxinc/nginx-unprivileged:1.21
-COPY --from=0 /opt/app-root/src/build /usr/share/nginx/html
-ADD ./nginx/nginx.conf /etc/nginx/nginx.conf
-ADD ./nginx/default.conf /etc/nginx/conf.d/default.conf
+
+# Copy built assets from builder stage
+COPY --from=builder /app/dist /usr/share/nginx/html
+
+# Copy Nginx configuration files
+COPY ./nginx/nginx.conf /etc/nginx/nginx.conf
+COPY ./nginx/default.conf /etc/nginx/conf.d/default.conf
--- gulpfile.js
+++ gulpfile.js
@@ -1,121 +1,116 @@
-var gulp = require('gulp'),
-	browserify = require('browserify'),
-	source = require('vinyl-source-stream'),
-	imagemin = require('gulp-imagemin'),
-	concatcss = require('gulp-concat-css'),
-	uglify = require('gulp-uglify');
-	rimraf = require('gulp-rimraf');
+const gulp = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const imagemin = require('gulp-imagemin');
+const concatcss = require('gulp-concat-css');
+const uglify = require('gulp-uglify');
+const { deleteAsync } = require('del');
 
-var browsersync = require('browser-sync').create();
-var reload = browsersync.reload;
+const browsersync = require('browser-sync').create();
+const reload = browsersync.reload;
 
 /** HELPER TASKS **/
 
-// Gulp Task: browserify
-// Build JavaScript using Browserify
-gulp.task('browserify', function() {
-	
-	gulp.src('./src/js/settings.js')
-		.pipe(gulp.dest('./build'))
-		.pipe(reload({stream: true}));
-		
-	return browserify('./src/js/main.js')
-		.exclude('./src/js/settings.js')
-		.bundle()
-		.pipe(source('main.js'))
-		.pipe(gulp.dest('./build'))
-		.pipe(reload({stream: true}));
-});
+// JavaScript bundling
+function browserifyTask() {
+    gulp.src('./src/js/settings.js')
+        .pipe(gulp.dest('./build'))
+        .pipe(reload({ stream: true }));
 
-// Gulp Task: watch
-// Automatic reloading with browser-sync
-gulp.task('watch', function() {
-	browsersync.init({
-		server:{
-			baseDir: './build'
-		}
-	});
-	
-	gulp.watch('./src/js/*.js',['browserify']);
-	gulp.watch('./src/php/*.php',['php']);
-	gulp.watch('./src/images/*.*',['images']);
-	gulp.watch('./src/css/*.css',['css']);
-	gulp.watch('./src/index.html',['html']);
-	gulp.watch('./src/tpl/*.html',['tpl']);
-	gulp.watch('./src/**/*.html',['html']);
-	
-	//gulp.watch(['/src/**/*.*'],['build']);
-	gulp.watch('./build/*.*').on('change',reload);
-});
+    return browserify('./src/js/main.js')
+        .exclude('./src/js/settings.js')
+        .bundle()
+        .pipe(source('main.js'))
+        .pipe(gulp.dest('./build'))
+        .pipe(reload({ stream: true }));
+}
 
-// Gulp Task: tpl
 // Page templates
-gulp.task('tpl', function() {
-	return gulp.src('./src/tpl/*.html')
-		.pipe(gulp.dest('./build/tpl/'))
-		.pipe(reload({stream: true}));
-});
+function tpl() {
+    return gulp.src('./src/tpl/*.html')
+        .pipe(gulp.dest('./build/tpl/'))
+        .pipe(reload({ stream: true }));
+}
 
-// Gulp Task: html
-// Optmized HTML
-gulp.task('html', function() {
-	return gulp.src('./src/index.html')
-		.pipe(gulp.dest('./build/'))
-		.pipe(reload({stream: true}));
-});
+// HTML compilation
+function html() {
+    return gulp.src('./src/index.html')
+        .pipe(gulp.dest('./build/'))
+        .pipe(reload({ stream: true }));
+}
 
-// Gulp Task: php
 // Copy PHP
-gulp.task('php', function() {
-	return gulp.src('./src/php/*.php')
-		.pipe(gulp.dest('./build/php/'))
-		.pipe(reload({stream: true}));
-});
+function php() {
+    return gulp.src('./src/php/*.php')
+        .pipe(gulp.dest('./build/php/'))
+        .pipe(reload({ stream: true }));
+}
 
-// Gulp Task: css
-// Concatenate CSS files
-gulp.task('css', function() {
-	return gulp.src('./src/css/**/*.css')
-		.pipe(concatcss('main.css'))
-		.pipe(gulp.dest('./build/'));
-});
+// Concatenate CSS
+function css() {
+    return gulp.src('./src/css/**/*.css')
+        .pipe(concatcss('main.css'))
+        .pipe(gulp.dest('./build/'));
+}
 
-// Gulp Task: images
-// Minify images and copy to build folder
-gulp.task('images', function() {
-	return gulp.src('./src/images/**')
-		.pipe(imagemin())
-		.pipe(gulp.dest('./build/images'))
-		.pipe(reload({stream: true}));
-});
+// Minify images
+function images() {
+    return gulp.src('./src/images/**')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'))
+        .pipe(reload({ stream: true }));
+}
 
-// Gulp Task: clean
-// Clean the dist folder
-gulp.task('clean', function() {
-	return gulp.src(['./dist/**/*.*','./build/**/*.*'],{read: false})
-		.pipe(rimraf());
-});
+// Clean output directories
+function clean() {
+    return deleteAsync(['./dist', './build']);
+}
 
+// Watch task
+function watch() {
+    browsersync.init({
+        server: {
+            baseDir: './build'
+        }
+    });
 
-/** RUN TASKS **/
+    gulp.watch('./src/js/*.js', browserifyTask);
+    gulp.watch('./src/php/*.php', php);
+    gulp.watch('./src/images/*.*', images);
+    gulp.watch('./src/css/*.css', css);
+    gulp.watch('./src/index.html', html);
+    gulp.watch('./src/tpl/*.html', tpl);
+    gulp.watch('./src/**/*.html', html);
 
-// Gulp Task: build
-// For local development
-gulp.task('build', ['html','tpl','css','php','images','browserify']);
+    gulp.watch('./build/*.*').on('change', reload);
+}
 
-// Gulp Task: dist
-// Create a production build
-gulp.task('dist', ['build'], function() {
-	gulp.src('./build/main.js')
-		.pipe(uglify())
-		.pipe(gulp.dest('./dist'));
-	
-	return gulp.src(['./build/**','!./build/main.js'])
-		.pipe(gulp.dest('./dist'));
-});
+// Dist task logic
+function distFiles() {
+    gulp.src('./build/main.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'));
 
-/*
-Gulp Task: default
-Default gulp task (no parameters)
-*/
-gulp.task('default', ['build','watch']);
+    return gulp.src(['./build/**', '!./build/main.js'])
+        .pipe(gulp.dest('./dist'));
+}
+
+/** COMPOSITE TASKS **/
+
+const build = gulp.parallel(html, tpl, css, php, images, browserifyTask);
+const dist = gulp.series(build, distFiles);
+const defaultTask = gulp.series(build, watch);
+
+/** TASK EXPORTS **/
+
+exports.browserify = browserifyTask;
+exports.tpl = tpl;
+exports.html = html;
+exports.php = php;
+exports.css = css;
+exports.images = images;
+exports.clean = clean;
+exports.watch = watch;
+exports.build = build;
+exports.dist = dist;
+exports.default = defaultTask;
